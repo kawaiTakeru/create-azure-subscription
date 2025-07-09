2fdeb6259836dd841b4636d2183628f9e1224aca
kawaiTakeru <t-kawai@bfts.co.jp>
2025-07-09 16:06:38 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,23 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_subscription" "new" {
+  billing_scope_id        = var.billing_scope_id
+  subscription_name       = var.subscription_display_name
+  workload                = "Production"
+  alias                   = "cr-sub-terraform"
+  display_name            = var.subscription_display_name
+}
 
