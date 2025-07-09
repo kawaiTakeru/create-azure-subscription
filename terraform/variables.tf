301e0c3826f6fa051bc783ad3502d1c5b10bb5bc
kawaiTakeru <t-kawai@bfts.co.jp>
2025-07-09 16:04:59 +0900

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,9 @@
+variable "subscription_display_name" {
+  description = "The display name of the new Azure subscription."
+  type        = string
+}
 
+variable "billing_scope_id" {
+  description = "The billing scope ID used when creating the subscription."
+  type        = string
+}
